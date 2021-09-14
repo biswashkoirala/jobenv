01737821ca678834865560e3ed0a4e70ab5221d9
Biswash Koirala <biswashkoirala2@gmail.com>
2021-09-14 14:59:58 +0545

    table created. solved import path issues

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,25 @@
 from fastapi import FastAPI
 from . core.config import settings
 from . apis.general_pages.route_homepage import general_pages_router
-
+from fastapi.staticfiles import StaticFiles
+from . db.session import engine  
+from . db.base import Base
 
 def include_router(app):
 	app.include_router(general_pages_router)
 
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="backend/static"), name="static")
+
+
+def create_tables():
+ 	Base.metadata.create_all(bind=engine)
 
 def start_application():
-	app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 	include_router(app)
+	configure_static(app)
+	create_tables()
 	return app 
 
 
