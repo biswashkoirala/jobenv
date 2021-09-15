b57967bfff1de86b0ca5681778431a2013fdbc41
Biswash Koirala <biswashkoirala2@gmail.com>
2021-09-15 18:04:25 +0545

    unit test

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,14 @@
 from fastapi import FastAPI
 from . core.config import settings
 from . apis.general_pages.route_homepage import general_pages_router
+from backend.apis.base import api_router
 from fastapi.staticfiles import StaticFiles
 from . db.session import engine  
 from . db.base import Base
 
 def include_router(app):
-	app.include_router(general_pages_router)
+	#app.include_router(general_pages_router)
+	app.include_router(api_router)
 
 def configure_static(app):
     app.mount("/static", StaticFiles(directory="backend/static"), name="static")
